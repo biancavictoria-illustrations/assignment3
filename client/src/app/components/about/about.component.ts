24a7b91efa1f7bc1bdb656906db574daeff8dfb9
bvictoria21 <biancavictoria2199@gmail.com>
2021-11-02 13:22:04 -0700

    Part 2 finished

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-about',
@@ -11,15 +12,24 @@ export class AboutComponent implements OnInit {
   profile_link:string = null;
 
   //TODO: inject the Spotify service
-  constructor() { }
+  constructor(private spotifyService:SpotifyService) { }
 
   ngOnInit() {
   }
-
   /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
   In that function, update the name, profile_pic, and profile_link fields 
   
   document.get <put info in html>
   */
+
+  aboutMe()
+  {
+    this.spotifyService.aboutMe().then((data) => {
+      this.name = data.name;
+      this.profile_link = data.spotifyProfile;
+      this.profile_pic = data.imageURL;
+      console.log(this.profile_link);
+    });
+  }
 
 }
--- client/src/app/services/spotify.service.ts
+++ client/src/app/services/spotify.service.ts
@@ -28,7 +28,7 @@ export class SpotifyService {
     //TODO: use the injected http Service to make a get request to the Express endpoint and return the response.
     //the http service works similarly to fetch(). It may be useful to call .toPromise() on any responses.
     //update the return to instead return a Promise with the data from the Express server
-    //return Promise.resolve();
+    //return Promise.resolve(link);
   }
 
   aboutMe():Promise<ProfileData> {
